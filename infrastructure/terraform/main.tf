c18edb6e6c0665da2db5a7c03a686766d0598cdc
adham-2002 <adham54732@gmail.com>
2025-04-16 09:39:32 +0200

    feat: Add Application Load Balancer and associated resources in Terraform configuration

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -29,7 +29,32 @@ data "aws_subnets" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP/HTTPS traffic to ALB"
+  vpc_id      = data.aws_vpc.default.id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # Define security group
 resource "aws_security_group" "tf-sg" {
@@ -77,4 +102,51 @@ resource "aws_instance" "kubeshop-tf" {
   tags = {
     Name = "my_ecommerce_server-${count.index}"
   }
+}
+# Define ALB 
+# Application Load Balancer
+resource "aws_lb" "ci_cd_pipeline" {
+  name               = "ci-cd-pipeline-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids
+
+  enable_deletion_protection = false
+}
+
+# Target Group
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Listener
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.ci_cd_pipeline.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# Target Group Attachments
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  count            = var.instance_count
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.kubeshop-tf[count.index].id
+  port             = 80
 }
